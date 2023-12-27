280fc1d0fe9b2b3e9da7f6f93e3da5efacc16d97
Bhavana851 <tjbhavana257@gmail.com>
2023-12-27 13:29:22 +0530

    no commit

---
+++ employees.py
@@ -0,0 +1,9 @@
+class Employees:
+    employee_id = 0
+employee1 = Employees()
+employee2 = Employees()
+
+employee1.employee_id=1001
+print(f"Employee Id:{employee1.employee_id}")
+employee2.employee_id=1002.
+print(f"Employee Id:{employee2.employee_id}")
